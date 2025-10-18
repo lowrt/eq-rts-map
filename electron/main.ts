fca6eac39429aae9bc517e83d8034eed3d8dc983
YuYu1015 <whes1015@gmail.com>
2025-10-18 21:07:20 +0800

    feat: webpack

---
+++ electron/main.ts
@@ -0,0 +1,49 @@
+import { app, BrowserWindow } from 'electron';
+import serve from 'electron-serve';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const loadURL = serve({ directory: 'out' });
+
+let mainWindow: BrowserWindow | null;
+
+(async () => {
+  await app.whenReady();
+
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.cjs'),
+    },
+  });
+
+  await loadURL(mainWindow);
+})();
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', async () => {
+  if (BrowserWindow.getAllWindows().length === 0 && mainWindow === null) {
+    mainWindow = new BrowserWindow({
+      width: 1200,
+      height: 800,
+      webPreferences: {
+        nodeIntegration: false,
+        contextIsolation: true,
+        preload: path.join(__dirname, 'preload.cjs'),
+      },
+    });
+
+    await loadURL(mainWindow);
+  }
+});
