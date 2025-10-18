9181bd0482f829c0ac95140686da77883b87331a
YuYu1015 <whes1015@gmail.com>
2025-10-18 17:15:44 +0800

    fix: build

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  // 為 Electron 打包設置正確的資源路徑
-  assetPrefix: './',
-  basePath: '',
 };
 
 export default nextConfig;
